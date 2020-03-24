25ba18c8e1d18c614633131783b1e401fa63b837
mohd fisha hafiz bin abidin <fisha.hafiz@moe.gov.my>
2020-03-24 08:18:09 +0800

    Update registerdataSupervisor.php

--- supervisorAFM/registerdataSupervisor.php
+++ supervisorAFM/registerdataSupervisor.php
@@ -439,7 +439,7 @@
         
         <div class="form-group">
             <div class="input-group mb-3">
-                <input type="hidden" name="stationCode" class="form-control" placeholder="<?echo $SC['name'];?>" value="<?php echo $stationSS;?>">
+                <input type="text" name="stationCode" class="form-control" value="<?php echo $stationSS;?>">
                    </div>
             </div>
         </div>
