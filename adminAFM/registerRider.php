177631f0ef0570039c8217cb3f4848b4e7de7e58
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-06-03 20:20:14 +0800

    Update registerRider.php

--- adminAFM/registerRider.php
+++ adminAFM/registerRider.php
@@ -700,7 +700,7 @@ $downloadExcell = $_SERVER['PHP_SELF'];
               <div class="card-header">
                 <h3 class="card-title">
                   <i class="fas fa-chart-bar mr-1"></i>
-                  Registered Rider/SV <p class="h6">(Data Shown by Month)</p>
+                  Registered Rider/SV <p class="h6"></p>
                 </h3>
               </div><!-- /.card-header -->
               <div class="card-body">
